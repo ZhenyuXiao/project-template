2236580f545e169e893e8b03c4a392f1aab5c872
Shaw <xiaozhenyu@iie.ac.cn>
2024-06-23 15:25:28 +0800

    初始化仓库

---
+++ README.md
@@ -0,0 +1,1 @@
+[gitignore binary files that have no extension](https://stackoverflow.com/questions/5711120/gitignore-binary-files-that-have-no-extension)
---
+++ src/Hello.cpp
@@ -0,0 +1,10 @@
+#include "Hello/Hello.h"
+#include <iostream>
+
+using std::cin, std::cout, std::endl;
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    cout << hello << endl;
+    return 0;
+}
