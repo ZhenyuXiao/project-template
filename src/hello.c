a17392218ec9d92f142be3cfc7f2ac5f6e4801df
Shaw <xiaozhenyu@iie.ac.cn>
2024-07-25 12:13:42 +0800

    测试C语言整数

--- src/hello.c
+++ src/hello.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <endian.h>
 
 union hello {
     int foo;
@@ -9,5 +12,7 @@ int main(int argc, char const *argv[]) {
     // 测试union如何初始化
     union hello foobar = {.bar = "Hello C!"};
     printf("%s\n", foobar.bar);
+    printf("%x\n", ((int32_t)2147483648 < 2147483647));
+    printf("%u\n", -(uint32_t)2147483648);
     return 0;
 }
